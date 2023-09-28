abd518f3c061fc12ef5042db07e0f8d75c1f5827
srikanth.bhompally33@gmail.com <surinenipurnachander@gmail.com>
2023-09-28 00:15:36 -0400

    destroying resources in aws

--- eks.tf
+++ eks.tf
@@ -73,7 +73,7 @@ resource "aws_iam_role" "fargate_execution_role" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "admin_policy_attachment" {
+resource "aws_iam_role_policy_attachment" "admin_policy_attachment2" {
   policy_arn = aws_iam_policy.admin_policy.arn
   role       = aws_iam_role.fargate_execution_role.name
 }
