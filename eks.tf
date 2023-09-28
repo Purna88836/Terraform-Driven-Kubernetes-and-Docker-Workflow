8732ba27376372d1f1e3a9f57fad730d979abaa6
srikanth.bhompally33@gmail.com <surinenipurnachander@gmail.com>
2023-09-27 20:59:27 -0400

    corrected eks.tf file

--- eks.tf
+++ eks.tf
@@ -26,8 +26,13 @@ resource "aws_iam_role" "my_cluster_role" {
 
 resource "aws_eks_fargate_profile" "default" {
   cluster_name = aws_eks_cluster.my_cluster.name
+  fargate_profile_name = "my-web-app"
   pod_execution_role_arn = aws_iam_role.fargate_execution_role.arn
   subnet_ids = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id]
+
+  selector {
+    namespace = "default"  # Replace with the namespace you want to target
+  }
 }
 
 resource "aws_iam_role" "fargate_execution_role" {
